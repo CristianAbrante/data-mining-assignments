069db2d120fa92ab17750f3287a4c27c03b28edd
Cristian Abrante <cristian@abrante.me>
2020-09-25 19:16:03 +0300

    Plots for silhouette added

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -14,6 +14,7 @@ import numpy as np
 
 DATA_FILE = "assignment-1/data/nba2013_data.csv"
 CATEGORICAL_COLUMNS = ['player', 'pos', 'bref_team_id', 'season', 'season_end']
+PLOT_FOLDER = "assignment-1/plots/"
 
 
 def preprocessing(data):
@@ -27,6 +28,52 @@ def preprocessing(data):
     return preprocessed_data
 
 
+def plot_silhouette_scores(n_clusters, data, labels, silhouette_avg, silhouette_values, file_name=None):
+    fig, ax = plt.subplots()
+    # Set limits for printing the silhouette.
+    ax.set_xlim(-0.2, 1)
+    ylim = len(data) + 10 * (n_clusters + 1)
+    ax.set_ylim(0, ylim)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        # Compute the ith silhouette value and sort for each value.
+        ith_cluster_silhouette_values = silhouette_values[labels == i]
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax.fill_betweenx(np.arange(y_lower, y_upper),
+                         0, ith_cluster_silhouette_values,
+                         facecolor=color, edgecolor=color, alpha=0.7)
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10
+
+    # Labels for axes
+    ax.set_xlabel("The silhouette coefficient values")
+    ax.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax.axvline(x=silhouette_avg, color="red", linestyle="--")
+    ax.text(silhouette_avg - 0.03, ylim + 5, str(np.round(silhouette_avg, 2)))
+
+    # Ticks for x and y axes
+    ax.set_yticks([])  # Clear the yaxis labels / ticks
+    ax.set_xticks([-0.2, -0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    plt.suptitle(("Silhouette analysis for K - Means clustering "
+                  "with k = %d" % n_clusters), fontweight='bold')
+
+    if file_name is not None:
+        plt.savefig(f'{PLOT_FOLDER}{file_name}')
+
+
 def exercise_1():
     data = pd.read_csv(DATA_FILE)
     data = preprocessing(data)
@@ -34,11 +81,6 @@ def exercise_1():
     range_n_clusters = [2, 5, 10]
 
     for n_clusters in range_n_clusters:
-        fig, ax = plt.subplots()
-        # Set limits for printing the silhouette.
-        ax.set_xlim(-0.1, 1)
-        ax.set_ylim(0, len(data) + 10 * (n_clusters + 1))
-
         km = KMeans(n_clusters=n_clusters, random_state=10)
         labels = km.fit_predict(data)
 
@@ -49,38 +91,8 @@ def exercise_1():
         # Silhouette score for each sample.
         silhouette_values = silhouette_samples(data, labels)
 
-        y_lower = 10
-        for i in range(n_clusters):
-            # Compute the ith silhouette value and sort for each value.
-            ith_cluster_silhouette_values = silhouette_values[labels == i]
-            ith_cluster_silhouette_values.sort()
-
-            size_cluster_i = ith_cluster_silhouette_values.shape[0]
-            y_upper = y_lower + size_cluster_i
-
-            color = cm.nipy_spectral(float(i) / n_clusters)
-            ax.fill_betweenx(np.arange(y_lower, y_upper),
-                             0, ith_cluster_silhouette_values,
-                             facecolor=color, edgecolor=color, alpha=0.7)
-
-            # Label the silhouette plots with their cluster numbers at the middle
-            ax.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-            # Compute the new y_lower for next plot
-            y_lower = y_upper + 10
-
-        # ax.set_title("The silhouette plot for the various clusters.")
-        ax.set_xlabel("The silhouette coefficient values")
-        ax.set_ylabel("Cluster label")
-
-        # The vertical line for average silhouette score of all the values
-        ax.axvline(x=silhouette_avg, color="red", linestyle="--")
-
-        ax.set_yticks([])  # Clear the yaxis labels / ticks
-        ax.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-        plt.suptitle(("Silhouette analysis for K - Means clustering "
-                      "with k = %d" % n_clusters), fontweight='bold')
+        plot_silhouette_scores(n_clusters, data, labels, silhouette_avg, silhouette_values,
+                               f'silhouette-plot-k-{n_clusters}.png')
 
     plt.show()
 
