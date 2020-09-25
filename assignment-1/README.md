940f663724bce6ed5bbc7b518fd4b46f7c996acd
Cristian Abrante <cristian@abrante.me>
2020-09-25 12:40:20 +0300

    Structure for assignment 1 created

---
+++ assignment-1/README.md
@@ -0,0 +1,3 @@
+# Assignment 1
+
+Description of assignment 1     
---
+++ assignment-1/main.py
@@ -0,0 +1,11 @@
+# Imports
+import pandas as pd
+
+
+def main():
+    data = pd.read_csv("assignment-1/data/nba2013_data.csv")
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
